8c9f9489ffb159a958f2227506f2e2ef994685ec
Alex Mustdie <epfly@ya.ru>
2020-02-07 01:09:31 +0300

    add readme & deploy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+
 from currency_converter import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
